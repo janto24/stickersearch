6ff2d3dc3fc7eb8edb4aa43a51e15000ff676eab
janto24 <jjanto24@gmail.com>
2023-05-17 12:29:58 -0300

    Avance

--- src/components/ApiSticks/ApiStick.js
+++ src/components/ApiSticks/ApiStick.js
@@ -1,37 +1,40 @@
 import { useState, useEffect } from "react";
+import { WhatsappShareButton, WhatsappIcon } from "react-share";
+import { Card } from '@mui/material/Card';
 
 const apiKey = "3JlB0CKggMuIxKZxdjmzCx0Fg3KjKfnk&q";
 
-const ApiSticks = ({search = `messi`} = {} ) => {
-  const [sticks, setSticks ] = useState([]);
+const ApiSticks = ({ search = "messi" }) => {
+  const [sticks, setSticks] = useState([]);
   const [inputText, setInputText] = useState("");
-  const [seach, setSearch] = useState(search);
+  const [searchTerm, setSearchTerm] = useState(search);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const handleClick = () => {
-    setSearch(inputText);
+    setSearchTerm(inputText);
   };
 
-  useEffect (() => {
-    const apiUrl = `https://api.giphy.com/v1/stickers/search?api_key=${apiKey}=${seach}&limit=25&offset=0&rating=g&lang=es`;
+  useEffect(() => {
+    const apiUrl = `https://api.giphy.com/v1/stickers/search?api_key=${apiKey}=${searchTerm}&limit=25&offset=0&rating=g&lang=es`;
     setIsLoading(true);
     setError(null);
+
     setTimeout(() => {
       fetch(apiUrl)
-        .then(res => res.json())
-        .then(response => {
-          const {data} = response;
-          const sticks = data.map(image => image.images.fixed_height_small.url);
-          setSticks(sticks);
+        .then((res) => res.json())
+        .then((response) => {
+          const { data } = response;
+          const stickUrls = data.map((image) => image.images.fixed_height_small.url);
+          setSticks(stickUrls);
           setIsLoading(false);
         })
-        .catch(error => {
+        .catch((error) => {
           setError(error.message);
           setIsLoading(false);
         });
     }, 5000);
-  }, [seach]);
+  }, [searchTerm]);
 
   return (
     <div className="container">
@@ -39,12 +42,24 @@ const ApiSticks = ({search = `messi`} = {} ) => {
       <button onClick={handleClick}>Buscar</button>
       {isLoading && <div>Cargando...</div>}
       {error && <div>{error}</div>}
+      
       <div>
         {sticks.map((stick, index) => (
-          <img key={index} src={stick} alt={`stick ${index}`} />
+          <div key={index}>
+            <img src={stick} alt={`stick ${index}`} />
+            <ShareButton stickUrl={stick} />
+          </div>
         ))}
       </div>
     </div>
+  );
+};
+
+const ShareButton = ({ stickUrl }) => {
+  return (
+    <WhatsappShareButton url={stickUrl}>
+      <WhatsappIcon round size={32} />
+    </WhatsappShareButton>
   );
 };
 
